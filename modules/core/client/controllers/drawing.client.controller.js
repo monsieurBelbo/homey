a7c57815a922f1f4f18a84416a88c0331f784522
Gonzalo Bellver <gonzalo.bellver@gmail.com>
2017-04-09 18:05:02 -0300

    Map with drawing tool

---
+++ modules/core/client/controllers/drawing.client.controller.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('core').controller('DrawingController', ['$scope',
+    function ($scope) {
+        var vm = this;
+        vm.onMapOverlayCompleted = function(e){
+            console.log(e);        //TODO(gb): Remove trace!!!
+            console.log(e.type);
+        };
+    }
+]);
